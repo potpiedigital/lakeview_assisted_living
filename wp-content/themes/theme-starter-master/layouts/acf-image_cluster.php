b37495cdabea66e5a25f3396279761e98715fbef
Mark Hayes <mark.m.hayes1@gmail.com>
2019-04-16 06:25:38 -0400

    adding in the images for the cluster

---
+++ wp-content/themes/theme-starter-master/layouts/acf-image_cluster.php
@@ -0,0 +1,8 @@
+<section>
+  <img class="top-image" src="<?php the_sub_field('top_image'); ?>" />
+  <img class="center-image" src="<?php the_sub_field('center_image'); ?>" />
+  <img class="faded-left-image" src="<?php the_sub_field('faded_left_image'); ?>" />
+  <img class="faded-right-image" src="<?php the_sub_field('faded_right_image'); ?>" />
+  <p><?php the_sub_field('image_copy'); ?>
+  <a href="<?php the_sub_field('image_cta_link') ?>"><?php the_sub_field('image_cta_text'); ?></a>
+</section>
