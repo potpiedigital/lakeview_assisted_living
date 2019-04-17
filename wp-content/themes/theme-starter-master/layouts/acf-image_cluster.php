8ecdeba239b705c8513e93fdfa0c8fe6c66feacf
Mark Hayes <mark.m.hayes1@gmail.com>
2019-04-16 20:23:10 -0400

    fixing the image cluster acf block

--- wp-content/themes/theme-starter-master/layouts/acf-image_cluster.php
+++ wp-content/themes/theme-starter-master/layouts/acf-image_cluster.php
@@ -1,5 +1,5 @@
 <section>
-  <img class="top-image" src="<?php the_sub_field('top_image'); ?>" />
+  <img class="top-image" src="<?php the_sub_field('top_image')['value']; ?>" />
   <img class="center-image" src="<?php the_sub_field('center_image'); ?>" />
   <img class="faded-left-image" src="<?php the_sub_field('faded_left_image'); ?>" />
   <img class="faded-right-image" src="<?php the_sub_field('faded_right_image'); ?>" />
