343e554e023f4e59c45760400149457641bae0c1
Mark Hayes <mark.m.hayes1@gmail.com>
2019-04-21 12:05:52 -0400

    adding in the styling for the latest, and adjusting some of the other styling. making the button style a global class.

--- wp-content/themes/theme-starter-master/layouts/acf-image_cluster.php
+++ wp-content/themes/theme-starter-master/layouts/acf-image_cluster.php
@@ -5,8 +5,8 @@
     <img class="faded-left-image" src="<?php the_sub_field('faded_left_image'); ?>" />
     <img class="faded-right-image" src="<?php the_sub_field('faded_right_image'); ?>" />
     <div class="image-info">
-      <h3><?php the_sub_field('image_copy'); ?></h3>
-      <a href="<?php the_sub_field('image_cta_link') ?>"><?php the_sub_field('image_cta_text'); ?></a>
+      <h4><?php the_sub_field('image_copy'); ?></h4>
+      <a class="button" href="<?php the_sub_field('image_cta_link') ?>"><?php the_sub_field('image_cta_text'); ?></a>
     </div>
   <div>
 </section>
--- wp-content/themes/theme-starter-master/layouts/acf-the_latests.php
+++ wp-content/themes/theme-starter-master/layouts/acf-the_latests.php
@@ -1,18 +1,20 @@
 <section>
-	<h2><?php the_sub_field('updates_title'); ?></h2>
-	<div>
-		<?php $posts = get_sub_field('updates_posts'); if( $posts ): ?>
-		<ul>
-		<?php foreach( $posts as $post): // IMPORTANT - variable must be called $post ?>
-			<?php setup_postdata($post); ?>
-			<div>
-			<div class="thumbnail-container"><?php the_post_thumbnail(); ?></div>
-				<h4><?php the_title(); ?></h4>
-				<p><?php the_excerpt(); ?></p>
-			</div>
-		<?php endforeach; ?>
-		</ul>
-		<?php wp_reset_postdata(); // IMPORTANT - reset the $post object so the rest of the page works correctly ?>
-		<?php endif; ?>
-	</div>
+  <div class="updates-wrapper">
+    <h2><?php the_sub_field('updates_title'); ?></h2>
+    <div>
+      <?php $posts = get_sub_field('updates_posts'); if( $posts ): ?>
+      <ul class="updates-cols">
+      <?php foreach( $posts as $post): // IMPORTANT - variable must be called $post ?>
+        <?php setup_postdata($post); ?>
+        <div class="updates-col updates">
+        <div class="thumbnail-container"><?php the_post_thumbnail(); ?></div>
+          <h4><?php the_title(); ?></h4>
+          <p><?php the_excerpt(); ?></p>
+        </div>
+      <?php endforeach; ?>
+      </ul>
+      <?php wp_reset_postdata(); // IMPORTANT - reset the $post object so the rest of the page works correctly ?>
+      <?php endif; ?>
+    </div>
+  </div>
 </section>
