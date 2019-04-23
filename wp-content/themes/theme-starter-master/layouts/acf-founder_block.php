d66e48d7003618ad84ee1b67481e3e5ab1ca7ead
Mark Hayes <mark.m.hayes1@gmail.com>
2019-04-23 06:27:01 -0400

    adding in the final styling for homepage desktop

--- wp-content/themes/theme-starter-master/layouts/acf-founder_block.php
+++ wp-content/themes/theme-starter-master/layouts/acf-founder_block.php
@@ -1,7 +1,11 @@
-<section>
+<section class="founder">
   <h2><?php the_sub_field('founder_title'); ?></h2>
-  <h5><?php the_sub_field('founder_name'); ?></h5>
-  <p><?php the_sub_field('founder_message'); ?><p>
-  <a href="<?php the_sub_field('founder_link') ?>"><?php the_sub_field('founder_link_text'); ?></a>
-  <img src="<?php the_sub_field('founder_image'); ?>" />
+  <div class="founder-content">
+    <div class="founder-info">
+      <h4><?php the_sub_field('founder_name'); ?></h4>
+      <p><?php the_sub_field('founder_message'); ?><p>
+      <a class="button" href="<?php the_sub_field('founder_link') ?>"><?php the_sub_field('founder_link_text'); ?></a>
+    </div>
+    <img src="<?php the_sub_field('founder_image'); ?>" />
+  </div>
 </section>
