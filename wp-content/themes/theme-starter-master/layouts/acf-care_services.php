6d51a1b32f0bbac6698973af973b389fbf23d179
Mark Hayes <mark.m.hayes1@gmail.com>
2019-04-24 21:05:12 -0400

    adding styling for internal pages.

--- wp-content/themes/theme-starter-master/layouts/acf-care_services.php
+++ wp-content/themes/theme-starter-master/layouts/acf-care_services.php
@@ -1,19 +1,23 @@
-<section>
+<section class="service-section">
 	<?php
 		// check if the repeater field has rows of data
 		if( have_rows('services') ):
         // display a sub field value
 	?>
-	<div>
+
 		<?php // loop through the rows of data
 			while ( have_rows('services') ) : the_row();
-		?>
-    <h3><?php the_sub_field('service_title'); ?></h3>
-    <p><?php the_sub_field('service_description'); ?></p>
-    <a href="<?php the_sub_field('service_link') ?>"><?php the_sub_field('service_link_text'); ?></a>
+    ?>
+    <article class="service-group">
+      <div>
+        <h3 class="service-title"><?php the_sub_field('service_title'); ?></h3>
+        <p><?php the_sub_field('service_description'); ?></p>
+        <a href="<?php the_sub_field('service_link') ?>"><?php the_sub_field('service_link_text'); ?></a>
+      </div>
+    </article>
 		<?php endwhile; ?>
 
-	</div>
+
 	<?php
 		else :
     	// no rows found
