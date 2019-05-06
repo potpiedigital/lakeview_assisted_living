9d001f72203094e244519a29127e5aca6a6b0d77
Mark Hayes <mark.m.hayes1@gmail.com>
2019-05-06 06:19:51 -0400

    adding in the styling for the last internal page.

--- wp-content/themes/theme-starter-master/layouts/acf-cares_info.php
+++ wp-content/themes/theme-starter-master/layouts/acf-cares_info.php
@@ -1,4 +1,4 @@
-<section>
-  <h2><?php the_sub_field('cares_title'); ?></h2>
+<section class="copy-block">
+  <h1><?php the_sub_field('cares_title'); ?></h1>
   <?php the_sub_field('cares_copy'); ?>
 </section>
--- wp-content/themes/theme-starter-master/layouts/acf-cares_services.php
+++ wp-content/themes/theme-starter-master/layouts/acf-cares_services.php
@@ -4,13 +4,17 @@
 		if( have_rows('cares') ):
         // display a sub field value
 	?>
-	<div>
+	<div class="service-types">
 		<?php // loop through the rows of data
 			while ( have_rows('cares') ) : the_row();
-		?>
-		<h4><?php the_sub_field('title'); ?></h4>
-    <p><?php the_sub_field('description'); ?></p>
+    ?>
+    <div clas="single-care">
+    <div class="overview">
+		  <h4><?php the_sub_field('title'); ?></h4>
+      <p><?php the_sub_field('description'); ?></p>
+    </div>
     <?php the_sub_field('list_items'); ?>
+    </div>
 		<?php endwhile; ?>
 	</div>
 	<?php
