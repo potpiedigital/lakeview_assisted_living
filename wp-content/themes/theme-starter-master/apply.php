80bc07dc808ef6a02c7b397d6a7c89758d15843b
Mark Hayes <mark.m.hayes1@gmail.com>
2019-05-23 21:19:43 -0400

    update to wordpress and fixing the jobs page

--- wp-content/themes/theme-starter-master/apply.php
+++ wp-content/themes/theme-starter-master/apply.php
@@ -9,11 +9,11 @@ Template Name: apply
 <main>
   <div class="hero-image" style="background-image: url(<?php the_field('apply_hero'); ?>"></div>
   <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
-	<div>
+	<div class="applications">
     <?php if(get_field('available_jobs')): ?>
-    <ul>
+    <ul class="job-details">
       <?php while(has_sub_field('available_jobs')): ?>
-      <li><h4><?php the_sub_field('job_title'); ?></h4> - <?php the_sub_field('short_job_description'); ?></li>
+      <li><h4><?php the_sub_field('job_title'); ?></h4><?php the_sub_field('short_job_description'); ?></li>
       <?php endwhile; ?>
     </ul>
     <?php endif; ?>
